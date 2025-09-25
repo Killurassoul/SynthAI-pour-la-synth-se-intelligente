788f5ff8b88149ffc61af7dc4138006723490983
Rassoulcode <rassoulgye@gmail.com>
2025-09-25 16:37:41 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -585,6 +585,7 @@ async def ask_question(request: Request, question: str = Form(...)):
 # ========================
 # LANCEMENT
 # ========================
+
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Railway définit le port via cette variable
+    uvicorn.run(app, host="0.0.0.0", port=port)
